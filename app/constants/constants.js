753b379cec91f0ae00270dcf82c4ec24fb0abfa2
Alejandro Nanez <alejonanez@gmail.com>
2016-06-25 22:41:28 -0500

    Completed workshop

--- app/constants/constants.js
+++ app/constants/constants.js
@@ -1,3 +1,3 @@
 export const WEATHER_API_KEY = 'c8cffee9248c82e52349a94f517435ec';
-export const WEATHER_API_URL = 'api.openweathermap.org/data/2.5/weather';
-export const IP_API_URL = 'ipinfo.io';
+export const WEATHER_API_URL = 'http://api.openweathermap.org/data/2.5/weather';
+export const IP_API_URL = 'http://ipinfo.io';
