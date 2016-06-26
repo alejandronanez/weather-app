3dc89d2988c249a997cbbf00ef112848a0e37a93
Alejandro Nanez <alejonanez@gmail.com>
2016-06-25 23:50:29 -0500

    Split things up

---
+++ app/helpers/__tests__/helpers.test.js
@@ -0,0 +1,8 @@
+import test from 'ava';
+
+test.todo('getCoords');
+test.todo('getWeather');
+test.todo('checkResponse');
+test.todo('filterData');
+test.todo('updateDOM');
+test.todo('catchError');
---
+++ app/helpers/helpers.js
@@ -0,0 +1,103 @@
+
+import {
+	WEATHER_API_KEY,
+	WEATHER_API_URL,
+} from 'constants/constants';
+
+import axios from 'axios';
+
+export function getCoords({ data }) {
+	const lat = parseFloat(data.loc.split(',')[0]);
+	const lon = parseFloat(data.loc.split(',')[1]);
+
+	return { lat, lon };
+}
+
+export function getWeather({ lat, lon }) {
+	const params = {
+		APPID: WEATHER_API_KEY,
+		lat,
+		lon
+	};
+
+	return axios.get(WEATHER_API_URL, { params });
+}
+
+export function checkResponse(response) {
+
+	if (response.status !== 200) {
+		throw new Error('Something went wrong :(');
+	}
+
+	return response;
+}
+
+export function farenheitToCelsius(temperature) {
+	return ((temperature - 32) * 5) / 9;
+}
+
+export function celsiusToFarenheit(temperature) {
+	return ((temperature * 9) / 5) + 32;
+}
+
+export function kelvinToCelsius(temperature) {
+	return temperature - 273.15;
+}
+
+export function filterData({ data }) {
+	const { temp: cityTemperature } = data.main;
+	const cityIcon = data.weather[0];
+	const {
+		name: cityName,
+		weather: cityWeather
+	} = data;
+
+	return {
+		cityTemperature: kelvinToCelsius(cityTemperature),
+		cityName,
+		cityWeather: cityWeather[0].description,
+		cityIcon: cityIcon.id
+	};
+}
+
+export function updateDOM({ cityTemperature, cityName, cityWeather, cityIcon }) {
+	let temperature = cityTemperature.toFixed(1);
+	const toCelsiusButton = document.querySelector('.js-to-celsius');
+	const toFarenheitButton = document.querySelector('.js-to-farenheit');
+	const temperatureElement = document.querySelector('.js-temperature');
+
+	document.querySelector('.js-city').textContent = cityName;
+	document.querySelector('.js-weather').textContent = cityWeather;
+	document.querySelector('.js-icon').className = `js-icon wi wi-owm-${cityIcon}`;
+	temperatureElement.textContent = `${temperature} Cº`;
+	toCelsiusButton.setAttribute('class', 'active');
+	toCelsiusButton.disabled = true;
+
+	toCelsiusButton.addEventListener('click', () => {
+		temperature = farenheitToCelsius(temperature).toFixed(1);
+		temperatureElement.textContent = `${temperature} Cº`;
+
+		toFarenheitButton.setAttribute('class', '');
+		toCelsiusButton.setAttribute('class', 'active');
+
+		toFarenheitButton.disabled = false;
+		toCelsiusButton.disabled = true;
+	});
+
+	toFarenheitButton.addEventListener('click', () => {
+		temperature = celsiusToFarenheit(temperature).toFixed(1);
+		temperatureElement.textContent = `${temperature} Fº`;
+
+		toFarenheitButton.setAttribute('class', 'active');
+		toCelsiusButton.setAttribute('class', '');
+
+		toFarenheitButton.disabled = true;
+		toCelsiusButton.disabled = false;
+	});
+}
+
+export function catchError(error) {
+	/* eslint-disable no-alert */
+	alert(error);
+	/* eslint-enable no-alert */
+}
